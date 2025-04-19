05a9ba54eaf83fb34d381dd0367dbc2adbda95e5
Yash Mittal <mittal.yash12@hotmail.com>
2025-04-19 16:24:49 +0530

    fix: base url in docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   url: 'https://veil.theflywheel.in',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/veil',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
