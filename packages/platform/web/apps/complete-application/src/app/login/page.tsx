797d858f72df71fbb28406c5c9489882fec8460f
Jiya Gupta <coderaholic21@gmail.com>
2025-04-26 17:43:46 +0530

    add home link to login singup pages ui

--- packages/platform/web/apps/complete-application/src/app/login/page.tsx
+++ packages/platform/web/apps/complete-application/src/app/login/page.tsx
@@ -92,10 +92,12 @@ export default function Login() {
       <div className="container relative h-screen px-8 flex flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <div className="w-8 h-8 bg-white rounded-md flex items-center justify-center mr-2"></div>
-            Veil
-          </div>
+          <Link href={"/"}>
+            <div className="relative w-fit z-20 flex items-center text-lg font-medium">
+              <div className="w-8 h-8 bg-white rounded-md flex items-center justify-center mr-2"></div>
+              Veil
+            </div>
+          </Link>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
